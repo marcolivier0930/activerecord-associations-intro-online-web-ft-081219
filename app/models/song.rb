359ee8eab816fb81dbd228f291d0e389f1304494
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-04 01:32:14 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
+belongs_to :genre 
 
+  def get_genre_name
+    self.genre.name
+  end
+  
+  def drake_made_this
+    self.artist = Artist.find_or_create_by(name: "Drake")
+  end
 end
