e385eb629143d010836c082138835c9f7707a5e0
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-04 01:28:52 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ActiveRecord::Base
+  def get_genre_of_first_song
+    self.songs.first.genre
+  end
+  
+  def song_sount
+    self.songs.count
+  end
+  
+  def genre_count
+    self.genres.count
+  end
 end
